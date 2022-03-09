d29c56385cab42c61b8f6c28473a3f0c9763700e
Srilatha <Srilatha@apples-MacBook-Pro.local>
2022-03-09 21:25:59 +0530

    added banner component

--- build/index.js
+++ build/index.js
@@ -1,4 +1,20 @@
 // @ts-nocheck
-import CustomButton from "./CustomButton/index";
-export { CustomButton };
+// import CustomButton from "./CustomButton/index";
+// export { CustomButton }
+import * as React from 'react';
+import { Banner } from 'react-native-paper';
+var CustomButton = function () {
+    var _a = React.useState(true), visible = _a[0], setVisible = _a[1];
+    return (React.createElement(Banner, { visible: visible, actions: [
+            {
+                label: 'Fix it',
+                onPress: function () { return setVisible(false); },
+            },
+            {
+                label: 'Learn more',
+                onPress: function () { return setVisible(false); },
+            },
+        ] }, "There was a problem processing a transaction on your credit card."));
+};
+export default CustomButton;
 //# sourceMappingURL=index.js.map
