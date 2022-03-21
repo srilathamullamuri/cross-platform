d444e2ad251352638b692513079202e21a7183da
Srilatha <Srilatha@apples-MacBook-Pro.local>
2022-03-21 14:39:44 +0530

    added video player

--- build/CustomButton/index.native.js
+++ build/CustomButton/index.native.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { View, Text, Image, Linking } from 'react-native';
 import { Banner } from 'react-native-paper';
+import VideoPlayer from 'react-native-video-player';
 var ButtonNative = function (props) {
     var _a = React.useState(true), visible = _a[0], setVisible = _a[1];
     var handlePress = function () {
@@ -23,6 +24,7 @@ var ButtonNative = function (props) {
                 },
             ] },
             React.createElement(View, null,
+                React.createElement(VideoPlayer, { video: { uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4' }, videoWidth: 1600, videoHeight: 900, thumbnail: { uri: 'https://i.picsum.photos/id/866/1600/900.jpg' } }),
                 props.image && React.createElement(Image, { style: { width: 100, height: 100 }, resizeMode: 'stretch', source: { uri: props.image } }),
                 props.message && React.createElement(Text, null, props.message),
                 props.link && React.createElement(Text, { style: { color: '#237aff', textDecorationLine: 'underline' }, onPress: function () { return Linking.openURL(props.link); } }, props.link)))));
--- src/CustomButton/index.web.tsx
+++ src/CustomButton/index.web.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Banner } from 'react-native-paper';
 import {View, Text, Image, Linking} from 'react-native';
+import VideoPlayer from 'react-native-video-player';
 
 
 interface Iprops {
@@ -41,6 +42,12 @@ const ButtonWeb : React.FunctionComponent<Iprops> = (props) => {
         ]}
        >
         <View>
+        <VideoPlayer
+    video={{ uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4' }}
+    videoWidth={1600}
+    videoHeight={900}
+    thumbnail={{ uri: 'https://i.picsum.photos/id/866/1600/900.jpg' }}
+/>
           {props.image && <Image style = {{ width: 100, height: 100 }} resizeMode={'stretch'} source={{uri: props.image}} />}
           {props.message && <Text>{props.message}</Text>}
           {props.link && <Text style={{color: '#237aff', textDecorationLine: 'underline'}} onPress={() => Linking.openURL(props.link)}>{props.link}</Text>}
