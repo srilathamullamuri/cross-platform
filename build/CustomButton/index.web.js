3d26409254d8bce25e2bb59c8135c199b470f35b
Srilatha <Srilatha@apples-MacBook-Pro.local>
2022-03-21 16:38:40 +0530

    added video player

--- build/CustomButton/index.web.js
+++ build/CustomButton/index.web.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { Banner } from 'react-native-paper';
 import { View, Text, Image, Linking } from 'react-native';
-import VideoPlayer from 'react-native-video-player';
 var ButtonWeb = function (props) {
     var _a = React.useState(true), visible = _a[0], setVisible = _a[1];
     var handlePress = function () {
@@ -24,7 +23,7 @@ var ButtonWeb = function (props) {
                 },
             ] },
             React.createElement(View, null,
-                props.video && React.createElement(VideoPlayer, { video: { uri: props.video }, videoWidth: 1600, videoHeight: 900, thumbnail: { uri: 'https://i.picsum.photos/id/866/1600/900.jpg' } }),
+                props.video && React.createElement("iframe", { width: 200, src: props.video }),
                 props.image && React.createElement(Image, { style: { width: 100, height: 100 }, resizeMode: 'stretch', source: { uri: props.image } }),
                 props.message && React.createElement(Text, null, props.message),
                 props.link && React.createElement(Text, { style: { color: '#237aff', textDecorationLine: 'underline' }, onPress: function () { return Linking.openURL(props.link); } }, props.link)))));
--- src/CustomButton/index.web.tsx
+++ src/CustomButton/index.web.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { Banner } from 'react-native-paper';
 import {View, Text, Image, Linking} from 'react-native';
-import VideoPlayer from 'react-native-video-player';
 
 
 interface Iprops {
@@ -43,12 +42,11 @@ const ButtonWeb : React.FunctionComponent<Iprops> = (props) => {
         ]}
        >
         <View>
-          {props.video && <VideoPlayer
-            video={{ uri: props.video }}
-            videoWidth={1600}
-            videoHeight={900}
-            thumbnail={{ uri: 'https://i.picsum.photos/id/866/1600/900.jpg' }}
-          />}
+          {props.video && <iframe
+          width={200}
+          src={props.video}
+          // type="video/mp4"
+        />}
           {props.image && <Image style = {{ width: 100, height: 100 }} resizeMode={'stretch'} source={{uri: props.image}} />}
           {props.message && <Text>{props.message}</Text>}
           {props.link && <Text style={{color: '#237aff', textDecorationLine: 'underline'}} onPress={() => Linking.openURL(props.link)}>{props.link}</Text>}
